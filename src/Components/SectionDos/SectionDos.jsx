ad04f4a1b48c59e11b5514cf68d783438f2f0254
Nico <lopeznnr@gmail.com>
2023-07-18 15:50:05 -0300

    cambie texto section dos

--- src/Components/SectionDos/SectionDos.jsx
+++ src/Components/SectionDos/SectionDos.jsx
@@ -7,7 +7,7 @@ const SectionDos = () => {
             <hr />
             <div className="d-flex flex-md-row flex-sm-column ">
                 <div>
-                    <p className='colorGris'><span className='cuadrado'>S</span>oy profesor de Brazilian Jiu Jitsu y facilitador de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Coordino, junto a un grupo de amigas, un espacio autogestivo cultural,de acrobacias y movimiento corporal en la ciudad de Quilmes Oeste, Buenos Aires. Mi pasion por las artes marciales y el movimiento me dieron una gran curiosidad y motivacion para seguir estudiando y sumando habilidades en mi vida. Y acompañar a otros/as a descubrirlas.</p>
+                    <p className='colorGris'><span className='cuadrado'>S</span>oy profesor de Brazilian Jiu Jitsu y facilitador de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Coordino, junto a un grupo de amigas, un espacio autogestivo cultural,de acrobacias y movimiento corporal en la ciudad de Quilmes Oeste, Buenos Aires. Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
                     <h2 className='titleBlack'>Mi educacion</h2>
                     <p className='colorGris'><span className='marronLetra bold pad'>En estos momentos</span> me encuentro cursando y estudiando en la Universidad Nacional Tecnologica (UTN) la diplomatura en Programacion Web Full Stack.</p>
                 </div>
