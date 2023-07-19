9be4aeebd3176c6ef762ca2edb6ddcc0817a1a7e
Nico <lopeznnr@gmail.com>
2023-07-19 19:30:57 -0300

    ultimo commit antes de revisar

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -6,10 +6,10 @@ const Inicio = () => {
     
     return (
         <>
-        <div className="d-flex justify-content-between height fondo">
-            <div id="inicio" className='ms-4 d-flex flex-column justify-content-center'>
-                <h1 className="empujar titleBrown fontSize mx-4 fs ">Hola mi nombre<br/> es Nico Lopez</h1>
-                <p className='gris mx-4 fontSizeB'>Estudiante de Desarrolo Web Front End <br/> en la Universidad Tecnologica Nacional</p>
+        <div className="d-flex justify-content-between height fondo ps-lg-5">
+            <div id="inicio" className='ms-5 d-flex flex-column justify-content-center'>
+                <h1 className="empujar titleBrown fontSize mx-4 pb-2">Hola<br/> mi nombre<br/> es Nico Lopez</h1>
+                <p className='gris mx-4 fontSizeB pb-2'>Estudiante de Desarrolo Web Front End <br/> en la Universidad Tecnologica Nacional</p>
                 <a href="#resumen"><button className="btn btn-primary color ms-4 border-0" href="#resumen">Sobre Mi</button></a> 
             </div>
             <div>
