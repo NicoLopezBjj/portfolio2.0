4fd8243961ada196de8bce85a8df122c5ae25adb
Nico <lopeznnr@gmail.com>
2023-07-19 18:08:23 -0300

    probando ruta imagen

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Inicio.css"
 const Inicio = () => {
-    const imageSrc="TpReact/mifotosinfondo.png"
+    const imageSrc="./mifotosinfondo.png"
     return (
         <>
         <div className="d-flex justify-content-between height fondo">
