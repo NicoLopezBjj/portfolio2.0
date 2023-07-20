1ab0e18cddd0a581274e0f35f72840102953a4b1
Nico <lopeznnr@gmail.com>
2023-07-20 02:19:12 -0300

    mejore el texto del hero y termine el trabajoDos

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -9,7 +9,7 @@ const Inicio = () => {
         <div className="d-flex justify-content-between height fondo ps-lg-5">
             <div id="inicio" className='ms-5 d-flex flex-column justify-content-center'>
                 <h1 className="empujar titleBrown fontSize mx-4 pb-2">Hola<br/> mi nombre<br/> es Nico Lopez</h1>
-                <p className='gris mx-4 fontSizeB pb-2'>Estudiante de Desarrolo Web Front End <br/> en la Universidad Tecnologica Nacional</p>
+                <p className='gris mx-4 fontSizeB pb-2'>Estudiante de Desarrolo Web Front <br/> End  en la Universidad Tecnologica Nacional</p>
                 <a href="#resumen"><button className="btn btn-primary color ms-4 border-0 px-4 py-2" href="#resumen">Sobre Mi</button></a> 
             </div>
             <div>
