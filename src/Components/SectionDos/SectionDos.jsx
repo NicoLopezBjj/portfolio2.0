eda8040362130b3d11b9789339f8c30df3dd3940
NicoLopezBjj <lopeznnr@gmail.com>
2024-01-15 11:27:08 -0300

    add imageCertificado

--- src/Components/SectionDos/SectionDos.jsx
+++ src/Components/SectionDos/SectionDos.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./SectionDos.css"
 const SectionDos = () => {
+    const imageCertificado = "./certificado.jpg"
+
     return(
         <div id="resumen" className="px-5 pt-4 mt-md-5">
             <h2 className='titleBrown'>Resumen sobre mi</h2>
@@ -9,8 +11,6 @@ const SectionDos = () => {
                 <div>
                     <p className='colorGris'><span className='cuadrado'>S</span>oy Desarrollador Full Stack, me especializo en React y Node. En base de datos prefiero las no relacionales, utilizando MongoDB como principal herramienta, y sin problema de adaptarme a base de datos relacionales como MySQL.<br/> 
                     Tambien soy profesor de Brazilian Jiu Jitsu y de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
-                    
-                    <p className='colorGris'><span className='marronLetra bold pad'>En estos momentos</span> me encuentro cursando y estudiando en la Universidad Nacional Tecnologica (UTN) la diplomatura en Programacion Web Full Stack.</p>
                 </div>
                 <div className='ps-md-4'>
                     <h2 className='titleBlack'>Mi carrera</h2>
@@ -19,6 +19,10 @@ const SectionDos = () => {
             </div>
             <div className='d-flex flex-column'>
             <h2 className='titleBlack text-center'>Mi educacion</h2>
+            <div className='d-flex justify-content-between'>
+            <p className='colorGris'><span className='marronLetra bold pad'>En estos momentos</span> me encuentro cursando y estudiando en la Universidad Nacional Tecnologica (UTN) la diplomatura en Programacion Web Full Stack.</p>
+            <img className='imgCertificado' src={imageCertificado} alt="" />
+            </div>
             </div>
         </div>
     )
