7a06cccef7d39f3697c60ba3edaf04add0587f08
NicoLopezBjj <lopeznnr@gmail.com>
2024-01-15 11:06:16 -0300

    changes in text and layout sectionDos

--- src/Components/SectionDos/SectionDos.jsx
+++ src/Components/SectionDos/SectionDos.jsx
@@ -7,14 +7,18 @@ const SectionDos = () => {
             <hr />
             <div className="d-flex flex-md-row flex-column text-justify  ">
                 <div>
-                    <p className='colorGris'><span className='cuadrado'>S</span>oy profesor de Brazilian Jiu Jitsu y facilitador de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Coordino, junto a un grupo de amigas, un espacio autogestivo cultural,de acrobacias y movimiento corporal en la ciudad de Quilmes Oeste, Buenos Aires. Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
-                    <h2 className='titleBlack'>Mi educacion</h2>
+                    <p className='colorGris'><span className='cuadrado'>S</span>oy Desarrollador Full Stack, me especializo en React y Node. En base de datos prefiero las no relacionales, utilizando MongoDB como principal herramienta, y sin problema de adaptarme a base de datos relacionales como MySQL.<br/> 
+                    Tambien soy profesor de Brazilian Jiu Jitsu y de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
+                    
                     <p className='colorGris'><span className='marronLetra bold pad'>En estos momentos</span> me encuentro cursando y estudiando en la Universidad Nacional Tecnologica (UTN) la diplomatura en Programacion Web Full Stack.</p>
                 </div>
                 <div className='ps-md-4'>
                     <h2 className='titleBlack'>Mi carrera</h2>
-                <p className='colorGris'> Hace 8 años practico Jiu Jitsu, soy cinturon Marron, profesor, competidor activo y arbitro profesional. Tambien participe y participo en multiples eventos nacionales de acroyoga. Mi objetivo para este año es terminar el curso en la UTN. Perfeccionarme y prepararme para en el 2024 trabajar como programador, para ganar experencia y seguir creciendo en el ambito laboral y profesional.</p>
+                <p className='colorGris'> Hace 8 años practico Jiu Jitsu, soy cinturon Marron, profesor, competidor activo y arbitro profesional. Tambien participe y participo en multiples eventos nacionales de acroyoga. Mi objetivo para este año 2024 es insertarme en el mundo laboral IT para perfeccionarme como programador, ganar experencia y seguir creciendo como profesional.</p>
                 </div>
+            </div>
+            <div className='d-flex flex-column'>
+            <h2 className='titleBlack text-center'>Mi educacion</h2>
             </div>
         </div>
     )
