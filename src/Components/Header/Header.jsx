02f29de5ad672d5b9259c6171f57845209cd1322
NicoLopezBjj <lopeznnr@gmail.com>
2024-02-07 12:33:36 -0300

    center section information about me

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -14,15 +14,15 @@ const Header = () => {
     }
 
     return(
-    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top marron ">
-        <div className="container-fluid justify-content-evenly ">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container-fluid ">
             <a className="navbar-brand mx-md-5 me-1">NLDev</a>
             <button className="navbar-toggler" type="button"  onClick={toggleMenu}
                     aria-expanded={isMenuOpen} aria-controls="navbarNavAltMarkup" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNavAltMarkup">
-                <div className="navbar-nav margin px-1 overFlow">
+                <div className="navbar-nav margin px-1 overFlow w-100">
                     <a className="nav-link " aria-current="page" href="#inicio" onClick={handleLinkClick} >Inicio</a>
                     <a className="nav-link " href="#resumen" onClick={handleLinkClick}>Sobre mi</a>
                     <a className="nav-link " href="#lastJob" onClick={handleLinkClick}>Proyectos</a>
--- src/Components/SectionDos/SectionDos.jsx
+++ src/Components/SectionDos/SectionDos.jsx
@@ -4,21 +4,20 @@ const SectionDos = () => {
     const imageCertificado = "./certificado.jpg"
 
     return(
-        <div id="resumen" className="px-5 pt-4 mt-md-5">
+        <div id="resumen" className="px-5 pt-4 mt-md-5 text-center">
             <h2 className='titleBrown'>Resumen sobre mi</h2>
             <hr />
-            <div className="d-flex flex-md-row flex-column text-justify  ">
+            <div className=" flex-md-row text-center text-justify  w-50 align-items-center centrado ">
                 <div>
-                    <p className='colorGris'><span className='cuadrado'>S</span>oy Desarrollador Full Stack, me especializo en React y Node. En base de datos prefiero las no relacionales, utilizando MongoDB como principal herramienta, y sin problema de adaptarme a base de datos relacionales como MySQL.<br/> 
+                    <p className='textoGris'><span>S</span>oy Desarrollador Full Stack, me especializo en React y Node. En base de datos prefiero las no relacionales, utilizando MongoDB como principal herramienta.<br/> 
                     Tambien soy profesor de Brazilian Jiu Jitsu y de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
                 </div>
-                <div className='ps-md-4 d-flex flex-column'>
+                <div className='ps-md-4 d-flex flex-column justify-content-center'>
                     <h2 className='titleBlack'>Mi carrera</h2>
-                    <p className='colorGris'> Hace 8 años practico Jiu Jitsu, soy cinturon Marron, profesor, competidor activo y arbitro profesional. Tambien participe y participo en multiples eventos nacionales de acroyoga. Mi objetivo para este año 2024 es insertarme en el mundo laboral IT para perfeccionarme como programador, ganar experencia y seguir creciendo como profesional.</p>
+                    <p className='textoGris'> Hace 8 años practico Jiu Jitsu, soy cinturon Marron, profesor, competidor activo y arbitro profesional. Tambien participe y participo en multiples eventos nacionales de acroyoga. Mi objetivo para este año 2024 es insertarme en el mundo laboral IT para perfeccionarme como programador, ganar experencia y seguir creciendo como profesional.</p>
                     <h2 className='titleBlack'>Mi educacion</h2>
-                    <p className='colorGris'><span className='marronLetra bold pad'>En estos momentos</span> me encuentro cursando y estudiando en la Universidad Nacional Tecnologica (UTN) la diplomatura en Programacion Web Full Stack.</p>
+                    <p className='textoGris'><span className='marronLetra bold pad'>Termine la Diplomatura en Programacion Web Full Stack</span> en la Universidad Tecnologica Nacional.</p>
                     <a href=""><button className="btn btn-primary color border-0 px-4 py-2 mb-2 x-large" href="#cv">Ver Certificado</button></a> 
-
                 </div>
             </div>
                     {/* <img className='imgCertificado' src={imageCertificado} alt="" /> */}
