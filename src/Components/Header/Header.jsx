a80ea9784f501c85f4b32c465e262dcdfe770c54
NicoLopezBjj <lopeznnr@gmail.com>
2024-02-15 12:32:23 -0300

    header changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,12 +21,12 @@ const Header = () => {
                     aria-expanded={isMenuOpen} aria-controls="navbarNavAltMarkup" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNavAltMarkup">
-                <div className="navbar-nav margin px-1 overFlow w-100">
-                    <a className="nav-link " aria-current="page" href="#inicio" onClick={handleLinkClick} >Inicio</a>
-                    <a className="nav-link " href="#resumen" onClick={handleLinkClick}>Sobre mi</a>
-                    <a className="nav-link " href="#lastJob" onClick={handleLinkClick}>Proyectos</a>
-                    <a className="nav-link " href="#contactame" onClick={handleLinkClick}>Contacto</a>
+            <div className={`justify-content-end collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNavAltMarkup">
+                <div className="d-flex justify-content-end margin mx-5 px-1 overFlow w-100">
+                    <a className="nav-link mx-2" aria-current="page" href="#inicio" onClick={handleLinkClick} >Inicio</a>
+                    <a className="nav-link mx-2" href="#resumen" onClick={handleLinkClick}>Sobre mi</a>
+                    <a className="nav-link mx-2" href="#lastJob" onClick={handleLinkClick}>Proyectos</a>
+                    <a className="nav-link mx-2" href="#contactame" onClick={handleLinkClick}>Contacto</a>
                 </div>
             </div>
                 <div className={`justify-content-end ${isMenuOpen ? "d-none" : "d-flex"}`}>
