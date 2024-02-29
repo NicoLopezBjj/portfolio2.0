50d793b3892b79098587397f0789472f3e8ee9c0
NicoLopezBjj <lopeznnr@gmail.com>
2024-02-29 13:25:53 -0300

    change in aboutme

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -9,13 +9,21 @@ const Inicio = () => {
     
     return (
         <>
-        <div className="d-flex justify-content-between  height fondo ps-lg-5">
+        <div className="d-flex justify-content-between height fondo ps-lg-5">
             <div id="inicio" className='ms-5 d-flex flex-column justify-content-center'>
-                <h1 className="empujar titleBrown fontSize mx-4 pb-2">Hola<br/> mi nombre<br/> es Nico Lopez</h1>
-                <p className='gris mx-4 fontSizeB pb-2'>Soy Developer Full Stack <br/> de la Universidad Tecnologica Nacional</p>
-                <div className='d-flex justify-content-between'>
-                    <a href="#resumen"><button className="btn btn-primary color ms-4 border-0 px-4 py-2 mb-2 x-large" href="#cv">Ver CV</button></a> 
-                    <Iconos/>
+                <h1 className="empujar titleBrown fontSize mx-4 pb-2">Nicolas Lopez</h1>
+                <p className='gris mx-4 fontSizeB pb-2'>Desarrollador Full Stack <br/> de la Universidad Tecnologica Nacional</p>
+                <div className='d-flex justify-content-between ms-lg-4'>
+                    <a href="#resumen"><button className="btn btn-primary color  border-0 px-4 py-2  x-large" href="#cv">Ver CV</button></a> 
+                    <a href="https://github.com/NicoLopezBjj" target='_blank' className='iconoHover'>
+                        <i className="bi bi-github black  me-lg-4 mx-2 newIcon"></i>    
+                    </a>
+                    <a href="https://www.linkedin.com/in/nicolas-lopez-648a11279/" className='iconoHover' target='_blank'>
+                         <i className="bi bi-linkedin black me-lg-4 me-2 newIcon "></i>
+                    </a>  
+                    <a href="https://wa.me/5491140865132" target="_blank" className='iconoHover'>
+                        <i className=" bi bi-whatsapp black me-lg-4 me-2 newIcon"></i>
+                    </a> 
                 </div>
             </div>
             <div>
--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -9,11 +9,10 @@ const SobreMi = () => {
             <hr />
             <div className=" flex-md-row text-center text-justify  w-50 align-items-center centrado ">
                 <div>
-                    <p className='textoGris'><span>S</span>oy Desarrollador Full Stack, me especializo en React y Node. En base de datos prefiero las no relacionales, utilizando MongoDB como principal herramienta.<br/> 
-                    Tambien soy profesor de Brazilian Jiu Jitsu y de Acroyoga, formado en la escuela FLAY(Formacion Latinoamericana de Acroyoga). Mi pasion por las artes marciales y el movimiento me impulsaron y motivaron a seguir estudiando y sumando herramientas en mi vida. Y acompañar a otros/as a descubrirlas.</p>
+                    <p className='textoGris'>Hola soy Nicolas Lopez, de la ciudad de Quilmes, provincia de Buenos Aires, Argentina. Soy Desarrollador Full Stack, comprometido a ofrecer soluciones de alta calidad en el ambito del desarrollo de software.<br/> 
+                    Programo en Javascript, utilizando React y Node como framework, junto a MongoDB como base de datos. Estoy en continuo aprendizaje y crecimiento como desarrollador y emocionado por participar y contribuir a proyectos innovadores y equipos de trabajo.</p>
                 </div>
                 <div className='ps-md-4 d-flex flex-column justify-content-center'>
-                    <h2 className='titleBlack'>Mi educacion</h2>
                     <p className='textoGris'><span className='marronLetra bold pad'>Termine la Diplomatura en Programacion Web Full Stack</span> en la Universidad Tecnologica Nacional.</p>
                     <a href=""><button className="btn btn-primary color border-0 px-4 py-2 mb-2 x-large" href="#cv">Ver Certificado</button></a> 
                 </div>
