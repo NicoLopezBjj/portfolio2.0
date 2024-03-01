ae243c1255fb842f29e4a2b4b20f9cab43d2e143
NicoLopezBjj <lopeznnr@gmail.com>
2024-03-01 10:01:58 -0300

    changes in inicio.jsx

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -9,14 +9,14 @@ const Inicio = () => {
     
     return (
         <>
-        <div className="d-flex justify-content-between height fondo ps-lg-5">
-            <div id="inicio" className='ms-5 d-flex flex-column justify-content-center'>
-                <h1 className="empujar titleBrown fontSize mx-4 pb-2">Nicolas Lopez</h1>
-                <p className='gris mx-4 fontSizeB pb-2'>Desarrollador Full Stack <br/> de la Universidad Tecnologica Nacional</p>
+        <div className="d-flex  height fondo  ancho">
+            <div id="inicio" className=' d-flex flex-column justify-content-center'>
+                <h1 className="empujar titleBrown  pb-2">Nicolas Lopez</h1>
+                <p className='gris mx-4 strong pb-2'>Desarrollador Full Stack</p>
                 <div className='d-flex justify-content-between ms-lg-4'>
-                    <a href="#resumen"><button className="btn btn-primary color  border-0 px-4 py-2  x-large" href="#cv">Ver CV</button></a> 
+                    <a href="#resumen"><button className="btn btn-primary color  border-0 px-3 py-2  x-large" href="#cv">Ver CV</button></a> 
                     <a href="https://github.com/NicoLopezBjj" target='_blank' className='iconoHover'>
-                        <i className="bi bi-github black  me-lg-4 mx-2 newIcon"></i>    
+                        <i className="bi bi-github black  ms-3 me-lg-4 mx-2 newIcon"></i>    
                     </a>
                     <a href="https://www.linkedin.com/in/nicolas-lopez-648a11279/" className='iconoHover' target='_blank'>
                          <i className="bi bi-linkedin black me-lg-4 me-2 newIcon "></i>
@@ -27,7 +27,7 @@ const Inicio = () => {
                 </div>
             </div>
             <div>
-                <img className="imagePerfil d-none d-lg-block" src={imagePerfil} alt="" />
+                <img className="imagePerfil d-flex align-items-center" src={imagePerfil} alt="" />
             </div>
         </div>
         </>
