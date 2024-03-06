38e0058457d5ddf4f20619d47f2f2e3cf27cb134
NicoLopezBjj <lopeznnr@gmail.com>
2024-03-06 12:24:33 -0300

    add links of certificado and cv

--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -15,8 +15,8 @@ const SobreMi = () => {
                 <div className='ps-md-4 d-flex flex-column justify-content-center text-center'>
                     <p className='textoGris'><span className='marronLetra bold pad'>Termine la Diplomatura en Programacion Web Full Stack</span> en la Universidad Tecnologica Nacional.</p>
                     <div className='d-flex'>
-                    <a href=""><button className="me-3 btn btn-primary color btnCv border-0 px-4 py-2 mb-2 x-large" href="#cv">Ver Certificado</button></a> 
-                    <a href="#resumen"><button className="btn btn-primary color btnCv px-3 py-2  border-0 x-large" href="#cv">Ver CV</button></a> 
+                    <a href="https://drive.google.com/file/d/17Te_6K4G1O00FRxH8zJcQTWRa_Z3qgf0/view?usp=sharing" target='_blank'><button className="me-3 btn btn-primary color btnCv border-0 px-4 py-2 mb-2 x-large" href="#cv">Ver Certificado</button></a> 
+                    <a href="https://drive.google.com/file/d/1o2KH8wX3FApiTNQFwe2NxT1Mez3xxJ8x/view?usp=sharing" target='_blank'><button className="btn btn-primary color btnCv px-3 py-2  border-0 x-large" href="#cv">Ver CV</button></a> 
                     </div>
                 </div>
             </div>
